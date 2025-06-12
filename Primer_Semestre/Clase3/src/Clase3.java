dabcc212a088a5781a9e60b2b93162e175cec6db
Sebastián Tarnovsky <maria.frisone86@gmail.com>
2025-06-12 12:50:50 +0200

    Teoría Clase 3

---
+++ Primer_Semestre/Clase3/src/Clase3.java
@@ -0,0 +1,32 @@
+//CONSTANTES
+
+public class Clase3 {
+    public static void main(String[] args) {
+
+        //DECLARAS UNA CONSTANTE CON LA PALABRA FINAL
+        final double PI = 3.1416;
+        final int mayoriaEdad = 18;
+
+        System.out.println("El valor de PI es: " + PI);
+        System.out.println("La edad mínima para ser adulto en Spain es: " + mayoriaEdad);
+
+        int numero1 = 5;
+        int numero2 = 6;
+        int suma = numero1 + numero2;
+
+        //OPERADORES RELACIONALES
+
+        int x = 10;
+        int y = 20;
+        System.out.println("¿Es x igual a y?: " + (x==y));
+        System.out.println("Es x diferente a y?: " + (x!=y));
+        System.out.println("Es x mayor a y?: " + (x>y));
+        System.out.println("Es x menor a y?: " + (x<y));
+
+        //OPERADORES LOGICOS: es aquel que combina expresiones lógicas
+
+        boolean a = true;
+        boolean b = false;
+
+    }
+}
