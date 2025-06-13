b4bf3bca2e9da1f1427a61d5b78af9c0619b5039
Sebastián Tarnovsky <maria.frisone86@gmail.com>
2025-06-13 17:54:12 +0200

    Ejercicios Clase 9, Arrays

--- Primer_Semestre/Clase9/src/Clase9.java
+++ Primer_Semestre/Clase9/src/Clase9.java
@@ -30,36 +30,5 @@ public class Clase9 {
         //hacer algo más complejo uso el bucle normal.
 
 //        Arrays.stream(numeros).forEach(num -> System.out.println("Elemento: " + num));
-
-// EJERCICIO 4
-
-//  //      Scanner entrada = new Scanner(System.in);
-//        int [] numeros ={10,25,30,16,58};
-//
-//        System.out.println("Introduce un número a buscar:");
-//        int buscado = entrada.nextInt();
-//
-//        boolean encontrado = false;
-//
-//        for(int i=0; i< numeros.length; i++){
-//
-//            if(numeros[i]== buscado){
-//                System.out.println("El número " + buscado + " está en la posición: " + (i+1));
-//                encontrado = true;
-//                break;
-//            }
-//        }
-//        if (!encontrado){
-//            System.out.println("Tu número no está en la lista");
-//        }
-        //EJERCICIO 5
-
-        int[] numeros = {10, 25, 30, 16, 58};
-
-        Arrays.sort(numeros);
-
-        for(int num : numeros){
-            System.out.println(num);
-        }
     }
 }
---
+++ Primer_Semestre/Clase9/src/Ejercicios9.java
@@ -0,0 +1,75 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ejercicios9 {
+
+    public static void main(String[] args) {
+//1. Declarar y mostrar
+//Declara un array de 5 números enteros, asígnales valores y muéstralos por pantalla.
+//        Scanner sc = new Scanner(System.in);
+//          int[] numeros = new int[5];
+//           int indice = 0;
+//           int suma = 0;
+//
+//            for (int i = 0; i < numeros.length; i++) {
+//                System.out.println("Introduce el número " + (i+1) + ":");
+//                numeros[i] = sc.nextInt();
+//
+//                //Suma todos los valores de un array de enteros y muestra el total.
+//            }
+//            for(int num: numeros){
+//                suma+= num;
+//            }
+//        System.out.println("La suma es: " + suma);
+
+            //Dado un array de enteros, muestra el valor más alto y el más bajo.
+//
+//        int [] numeros ={10,25,30,16,58};
+//        int maximo = numeros[0];
+//        int minimo = numeros[0];
+//
+//        for(int i = 1; i<numeros.length; i++){
+//            if (numeros[i]> maximo){
+//                maximo = numeros[i];
+//            }
+//            if (numeros[i]< minimo){
+//                minimo = numeros[i];
+//            }
+//        }
+//        System.out.println("El numero mayor es: " + maximo + " y el mínimo es: " + minimo);
+
+
+        // Pide al usuario un número y muestra si está presente en un array, y en qué posición.
+
+//       Scanner entrada = new Scanner(System.in);
+//        int [] numeros ={10,25,30,16,58};
+//
+//        System.out.println("Introduce un número a buscar:");
+//        int buscado = entrada.nextInt();
+//
+//        boolean encontrado = false;
+//
+//        for(int i=0; i< numeros.length; i++){
+//
+//            if(numeros[i]== buscado){
+//                System.out.println("El número " + buscado + " está en la posición: " + (i+1));
+//                encontrado = true;
+//                break;
+//            }
+//        }
+//        if (!encontrado){
+//            System.out.println("Tu número no está en la lista");
+//        }
+
+        //Declara un array con números desordenados y ordénalo de menor a mayor utilizando
+        //Arrays.sort().
+
+//        int[] numeros = {10, 25, 30, 16, 58};
+//
+//        Arrays.sort(numeros);
+//
+//        for(int num : numeros){
+//            System.out.println(num);
+//        }
+    }
+}
